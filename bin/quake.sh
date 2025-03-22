afdb9610d920ffad924a0d48a43e667ca00b14ae
Jordan Sissel <jls@semicomplete.com>
2025-03-22 15:37:51 -0700

    Set container names. Also set a default quake game mode (baseq3)

--- bin/quake.sh
+++ bin/quake.sh
@@ -5,4 +5,5 @@ while ! curl -q -o /dev/null http://assets:9000/assets/manifest.json; do
   sleep 1
 done
 
-node build/ioq3ded.js +set fs_game baseq3 set dedicated 1 +exec server.cfg +set fs_cdn assets:9000
+QUAKE_GAME="${QUAKE_GAME:-baseq3}"
+node build/ioq3ded.js +set fs_game "${QUAKE_GAME}" set dedicated 1 +exec server.cfg +set fs_cdn assets:9000
