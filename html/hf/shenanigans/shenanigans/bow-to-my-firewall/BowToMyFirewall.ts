f5154126b44c777e041084a06e61eff7900bd27c
Jordan Sissel <jls@semicomplete.com>
2025-05-19 23:28:19 -0700

    Move shenanigans to one place.

---
+++ html/hf/shenanigans/shenanigans/bow-to-my-firewall/BowToMyFirewall.ts
@@ -0,0 +1,35 @@
+const elementId = "bow-to-my-firewall";
+
+let setupInvoked = false;
+export function Setup () {
+  if (setupInvoked) {
+    console.warn("Setup already invoked.");
+    return;
+  }
+
+  const html = `<div id="${elementId}" class="talk-box hide">
+      <img class="portrait" src="/hf/shenanigans/bow-to-my-firewall/bruce-firewall.png">
+      <div class="content">Bow to my firewall!</div>
+    </div>`;
+
+  const fragment = document.createRange().createContextualFragment(html);
+  document.body.append(fragment);
+  setupInvoked = true;
+}
+
+type Aagghhh = "Aaagghhh";
+export function Begin(aagghhh: Aagghhh) {
+  let element;
+  if ((element = document.getElementById(elementId)) !== null) {
+    element.classList.add("show", "shake");
+    element.classList.remove("hide");
+  }
+}
+
+export function End() {
+  let element;
+  if ((element = document.getElementById(elementId)) !== null) {
+    element.classList.add("hide");
+    element.classList.remove("show", "shake");
+  }
+}
