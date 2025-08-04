f1e1184fd35b4fce44060d17d7a5ce6e637cd6e0
Jordan Sissel <jls@semicomplete.com>
2025-08-04 14:46:36 -0700

    Move javascript/typescript code outside of html
    The plan is to add more things here like media we will use to build
    extra pk3 files (changing quake sounds, models, etc)

--- hf/shenanigans/bow-to-my-firewall/index.ts
+++ hf/shenanigans/bow-to-my-firewall/index.ts
@@ -18,11 +18,22 @@ export function Setup () {
 }
 
 type Aagghhh = "Aaagghhh";
+
+let timer: number | null = null;
 export function Begin(aagghhh: Aagghhh) {
   let element;
   if ((element = document.getElementById(elementId)) !== null) {
     element.classList.add("show", "shake");
     element.classList.remove("hide");
+
+    // Quad damage can end in-game when the player dies, drops the powerup(?),
+    // or the powerup expires.
+    // Until I figure out how to trigger End() when quad damage ends in the q3a client,
+    // just set a short timer which calls End()
+    if (timer !== null) {
+      window.clearTimeout(timer);
+    }
+    timer = window.setTimeout(End, 5000);
   }
 }
 
@@ -31,5 +42,9 @@ export function End() {
   if ((element = document.getElementById(elementId)) !== null) {
     element.classList.add("hide");
     element.classList.remove("show", "shake");
+    if (timer !== null) {
+      window.clearTimeout(timer);
+      timer = null;
+    }
   }
 }
