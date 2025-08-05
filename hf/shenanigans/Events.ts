9e05457114e7631204654240522f1b306eb16cf9
Jordan Sissel <jls@semicomplete.com>
2025-08-04 18:36:05 -0700

    Add quad damage events

--- hf/shenanigans/Events.ts
+++ hf/shenanigans/Events.ts
@@ -4,6 +4,8 @@ export enum TYPES {
   YouDied = "you died", // You died, normally.
   YouGibbed = "you got gibbed", // You died, but was gibbed.
   YouTeleported = "you teleported", // Walked through a teleporter
+  QuadDamageBegin = "quad damage begin",
+  QuadDamageEnd = "quad damage end",
 }
 
 // This event is invoked when the player dies, via g_combat.c's player_die
@@ -19,10 +21,19 @@ export function YouTeleported() {
   event.dispatchEvent(new CustomEvent(TYPES.YouTeleported));
 }
 
+export function QuadDamageBegin() {
+  event.dispatchEvent(new CustomEvent(TYPES.QuadDamageBegin));
+}
+export function QuadDamageEnd() {
+  event.dispatchEvent(new CustomEvent(TYPES.QuadDamageEnd));
+}
+
 import * as MirrorImage from "./mirror-image/index.js"
+import * as BowToMyFirewall from "./bow-to-my-firewall/index.js"
 
 event.addEventListener(TYPES.YouDied, () => { console.log("You died"); });
 event.addEventListener(TYPES.YouGibbed, () => { console.log("You died (gibbed)"); });
-event.addEventListener(TYPES.YouTeleported, () => { 
-  MirrorImage.Begin()
-});
+event.addEventListener(TYPES.YouTeleported, MirrorImage.Begin);
+
+event.addEventListener(TYPES.QuadDamageBegin, () => BowToMyFirewall.Begin("Aaagghhh"));
+event.addEventListener(TYPES.QuadDamageEnd, BowToMyFirewall.End);
