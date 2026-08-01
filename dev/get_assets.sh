d4d2c81814e9ee2f0789f367d261fa6aa69c32ec
Jordan Sissel <jls@semicomplete.com>
2026-07-31 22:42:58 -0700

    and debugging on github actions begins.

--- dev/get_assets.sh
+++ dev/get_assets.sh
@@ -38,7 +38,8 @@ cd hf
 
 # Fetch high-res asset replacements
 if ! echo  "8b703ea3ebe73da409d154f8e14cb836  pak90.pk3" | md5sum -c > /dev/null 2>&1 ; then
-	curl -C - --retry 3 --retry-delay 10 -O "https://files.ioquake3.org/xcsv_hires.zip"
+	curl -L -C - --retry 3 --retry-delay 10 -O "https://files.ioquake3.org/xcsv_hires.zip"
+	ls -l xcsv_hires.zip
 	unzip xcsv_hires.zip xcsv_bq3hi-res.pk3
 	mv xcsv_bq3hi-res.pk3 pak090.pk3
 	rm xcsv_hires.zip
